ae5da3d85d50850cec6b862d4562e6858ea3100e
Dominik1401 <78111584+Dominik1401@users.noreply.github.com>
2021-01-27 23:05:18 +0100

    Update cl_hud.lua

--- cl_hud.lua
+++ cl_hud.lua
@@ -5,47 +5,44 @@ surface.CreateFont ( "HudFont", {
 	weight = 500
 })
 		
-		--HP + Arm--	
-
 	hook.Add( "HUDPaint", "Hud", function()
 
-		local scrw,scrh = ScrW(), ScrH()
 		local ply = LocalPlayer()
 		local hp = ply:Health()
 		local maxhp = ply:GetMaxHealth()
-		local boxW = scrw * .1
-		local boxH = scrh * .02
+		local boxW = ScrW() * .1
+		local boxH = ScrH() * .02
 			surface.SetDrawColor(0,0,0,100)
-			surface.DrawRect(scrw / 2 - boxW / 2,scrh - boxH * 1.1, boxW, boxH)
+			surface.DrawRect(ScrW() / 2 - boxW / 2,ScrH() - boxH * 1.1, boxW, boxH)
 			surface.SetDrawColor(255,0,0,255)
-			surface.DrawRect(scrw / 2 - boxW / 2,scrh - boxH * 1.1, boxW * (hp / maxhp), boxH)
+			surface.DrawRect(ScrW() / 2 - boxW / 2,ScrH() - boxH * 1.1, boxW * (hp / maxhp), boxH)
 				surface.SetFont( "HudFont" )
 				surface.SetTextColor( 255, 255, 255, 200 )
-				surface.SetTextPos( scrw / 1.99 - boxW / 2,scrh - boxH * 1.2, boxW, boxH )
+				surface.SetTextPos( ScrW() / 1.99 - boxW / 2,ScrH() - boxH * 1.2, boxW, boxH )
 				surface.DrawText( hp )
 
 
 		local arm = ply:Armor()
 		local maxarm = ply:GetMaxArmor()
-		local boxW = scrw * .1
-		local boxH = scrh * .02
+		local boxW = ScrW() * .1
+		local boxH = ScrH() * .02
 			surface.SetDrawColor(0,0,0,100)
-			surface.DrawRect(scrw / 2 - boxW / 2,scrh - boxH * 2.5, boxW, boxH)
+			surface.DrawRect(ScrW() / 2 - boxW / 2,ScrH() - boxH * 2.5, boxW, boxH)
 			surface.SetDrawColor(0,100,255,255)
-			surface.DrawRect(scrw / 2 - boxW / 2,scrh - boxH * 2.5, boxW * (arm / maxarm), boxH)	
+			surface.DrawRect(ScrW() / 2 - boxW / 2,ScrH() - boxH * 2.5, boxW * (arm / maxarm), boxH)	
 				surface.SetFont( "HudFont" )
 				surface.SetTextColor( 255, 255, 255, 200 )
-				surface.SetTextPos( scrw / 1.99 - boxW / 2,scrh - boxH * 2.535, boxW, boxH )
+				surface.SetTextPos( ScrW() / 1.99 - boxW / 2,ScrH() - boxH * 2.535, boxW, boxH )
 				surface.DrawText( arm )
 			
 
 			surface.SetDrawColor(0,0,0,100)
-			surface.DrawRect(scrw / 2 - boxW / 2,scrh - boxH * 3.9, boxW, boxH)
+			surface.DrawRect(ScrW() / 2 - boxW / 2,ScrH() - boxH * 3.9, boxW, boxH)
 			surface.SetDrawColor(205,205,0,205)
-			surface.DrawRect(scrw / 2 - boxW / 2,scrh - boxH * 3.9, boxW, boxH)	
+			surface.DrawRect(ScrW() / 2 - boxW / 2,ScrH() - boxH * 3.9, boxW, boxH)	
 				surface.SetFont( "HudFont" )
 				surface.SetTextColor( 255, 255, 255, 200 )
-				surface.SetTextPos( scrw / 1.99 - boxW / 2,scrh - boxH * 4, boxW, boxH )
+				surface.SetTextPos( ScrW() / 1.99 - boxW / 2,ScrH() - boxH * 4, boxW, boxH )
 				surface.DrawText( "Stamina" )
 
 

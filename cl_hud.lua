d285ddf85936b15d535b69f4bd5a42cd93264392
Dominik1401 <78111584+Dominik1401@users.noreply.github.com>
2021-01-28 23:12:10 +0100

    Update cl_hud.lua

--- cl_hud.lua
+++ cl_hud.lua
@@ -46,16 +46,21 @@ hook.Add( "HUDPaint", "Hud", function()
 	local maxhp = ply:GetMaxHealth()
 	local boxW = ScrW() * .1
 	local boxH = ScrH() * .02
-		if ply:Alive() == -1 then return end
-		if ply:InVehicle() then return end
-			surface.SetDrawColor(0,0,0,100)
-			surface.DrawRect(ScrW() / 2 - boxW / 2,ScrH() - boxH * 1, boxW, boxH)
-			surface.SetDrawColor(255,0,0,255)
-			surface.DrawRect(ScrW() / 2 - boxW / 2,ScrH() - boxH * 1, boxW * (hp / maxhp), boxH)
-				surface.SetFont( "HudFont" )
-				surface.SetTextColor( 255, 255, 255, 200 )
-				surface.SetTextPos( ScrW() / 1.99 - boxW / 2,ScrH() - boxH * 1, boxW, boxH )
-				surface.DrawText( hp )
+
+	if ply:Alive() == -1 then return end
+	
+	if ply:InVehicle() then return end
+
+	surface.SetDrawColor(0,0,0,100)
+	surface.DrawRect(ScrW() / 2 - boxW / 2,ScrH() - boxH * 1, boxW, boxH)
+
+	surface.SetDrawColor(255,0,0,255)
+	surface.DrawRect(ScrW() / 2 - boxW / 2,ScrH() - boxH * 1, boxW * (hp / maxhp), boxH)
+
+	surface.SetFont( "HudFont" )
+	surface.SetTextColor( 255, 255, 255, 200 )
+	surface.SetTextPos( ScrW() / 1.99 - boxW / 2,ScrH() - boxH * 1, boxW, boxH )
+	surface.DrawText( hp )
 
 	-- armor --
 
@@ -63,27 +68,33 @@ hook.Add( "HUDPaint", "Hud", function()
 	local maxarm = ply:GetMaxArmor()
 	local boxW = ScrW() * .1
 	local boxH = ScrH() * .02
-		surface.SetDrawColor(0,0,0,100)
-		surface.DrawRect(ScrW() / 2 - boxW / 2,ScrH() - boxH * 2.5, boxW, boxH)
-		surface.SetDrawColor(0,100,255,255)
-		surface.DrawRect(ScrW() / 2 - boxW / 2,ScrH() - boxH * 2.5, boxW * (arm / maxarm), boxH)	
-			surface.SetFont( "HudFont" )
-			surface.SetTextColor( 255, 255, 255, 200 )
-			surface.SetTextPos( ScrW() / 1.99 - boxW / 2,ScrH() - boxH * 2.5, boxW, boxH )
-			surface.DrawText( arm )
+
+	surface.SetDrawColor(0,0,0,100)
+	surface.DrawRect(ScrW() / 2 - boxW / 2,ScrH() - boxH * 2.5, boxW, boxH)
+
+	surface.SetDrawColor(0,100,255,255)
+	surface.DrawRect(ScrW() / 2 - boxW / 2,ScrH() - boxH * 2.5, boxW * (arm / maxarm), boxH)
+	
+	surface.SetFont( "HudFont" )
+	surface.SetTextColor( 255, 255, 255, 200 )
+	surface.SetTextPos( ScrW() / 1.99 - boxW / 2,ScrH() - boxH * 2.5, boxW, boxH )
+	surface.DrawText( arm )
 
 		-- stamina --
 			
 	local stamina = ply:Health()
 	local maxstamina = ply:GetMaxHealth()
-		surface.SetDrawColor(0,0,0,100)
-		surface.DrawRect(ScrW() / 2 - boxW / 2,ScrH() - boxH * 4, boxW, boxH)
-		surface.SetDrawColor(205,205,0,205)
-		surface.DrawRect(ScrW() / 2 - boxW / 2,ScrH() - boxH * 4, boxW, boxH)	
-			surface.SetFont( "HudFont" )
-			surface.SetTextColor( 255, 255, 255, 200 )
-			surface.SetTextPos( ScrW() / 1.99 - boxW / 2,ScrH() - boxH * 4, boxW, boxH )
-			surface.DrawText( "Stamina" )
+
+	surface.SetDrawColor(0,0,0,100)
+	surface.DrawRect(ScrW() / 2 - boxW / 2,ScrH() - boxH * 4, boxW, boxH)
+
+	surface.SetDrawColor(205,205,0,205)
+	surface.DrawRect(ScrW() / 2 - boxW / 2,ScrH() - boxH * 4, boxW, boxH)
+	
+	surface.SetFont( "HudFont" )
+	surface.SetTextColor( 255, 255, 255, 200 )
+	surface.SetTextPos( ScrW() / 1.99 - boxW / 2,ScrH() - boxH * 4, boxW, boxH )
+	surface.DrawText( "Stamina" )
 
 		--CharInfo--
 
@@ -91,14 +102,17 @@ hook.Add( "HUDPaint", "Hud", function()
 	local Clone_idVar = ply:GetClassID()
 	local boxW = ScrW() * .2
 	local boxH = ScrH() * .08
-		surface.SetDrawColor(0,0,0,100)
-		surface.DrawRect(ScrW() / 1 - boxW / 1,ScrH() - boxH * 6, boxW, boxH)
-		surface.SetFont( "Name" )
-		surface.SetTextColor( 255, 255, 255, 200 )
-		surface.SetTextPos( ScrW() / .99 - boxW / 1,ScrH() - boxH * 5.7, boxW, boxH)
-		surface.DrawText( NameVar )
-		surface.SetTextPos( ScrW() / .9 - boxW / 1,ScrH() - boxH * 5.7, boxW, boxH)
-		surface.DrawText( Clone_idVar )
+
+	surface.SetDrawColor(0,0,0,100)
+	surface.DrawRect(ScrW() / 1 - boxW / 1,ScrH() - boxH * 6, boxW, boxH)
+
+	surface.SetFont( "Name" )
+	surface.SetTextColor( 255, 255, 255, 200 )
+	surface.SetTextPos( ScrW() / .99 - boxW / 1,ScrH() - boxH * 5.7, boxW, boxH)
+	surface.DrawText( NameVar )
+
+	surface.SetTextPos( ScrW() / .9 - boxW / 1,ScrH() - boxH * 5.7, boxW, boxH)
+	surface.DrawText( Clone_idVar )
 
 
 		-- ammo --
@@ -106,18 +120,23 @@ hook.Add( "HUDPaint", "Hud", function()
 	local ClipVar = ply:GetActiveWeapon():Clip1() 
 	local AmmoVar = ply:GetAmmoCount( ply:GetActiveWeapon():GetPrimaryAmmoType() )
 	local NameVar = ply:GetActiveWeapon():GetPrintName()
-		if ClipVar == -1 then return end
-			surface.SetDrawColor(0,0,0,100)
-			surface.DrawRect(ScrW() / 1 - boxW / 1,ScrH() - boxH * 1, boxW, boxH)
-				surface.SetFont( "Ammo" )
-				surface.SetTextColor( 255, 255, 255, 200 )
-				surface.SetTextPos( ScrW() / .985 - boxW / 1,ScrH() - boxH * .8, boxW, boxH)
-				surface.DrawText( ClipVar )
-				surface.SetTextPos( ScrW() / .905 - boxW / 1,ScrH() - boxH * .8, boxW, boxH)
-				surface.DrawText( AmmoVar )
-				surface.SetFont( "Weapon" )
-				surface.SetTextPos( ScrW() / .985 - boxW / 1,ScrH() - boxH * 1.4, boxW, boxH)
-				surface.DrawText( NameVar )
+
+	if ClipVar == -1 then return end
+
+	surface.SetDrawColor(0,0,0,100)
+	surface.DrawRect(ScrW() / 1 - boxW / 1,ScrH() - boxH * 1, boxW, boxH)
+			
+	surface.SetFont( "Ammo" )
+	surface.SetTextColor( 255, 255, 255, 200 )
+	surface.SetTextPos( ScrW() / .985 - boxW / 1,ScrH() - boxH * .8, boxW, boxH)
+	surface.DrawText( ClipVar )
+
+	surface.SetTextPos( ScrW() / .905 - boxW / 1,ScrH() - boxH * .8, boxW, boxH)
+	surface.DrawText( AmmoVar )
+
+	surface.SetFont( "Weapon" )
+	surface.SetTextPos( ScrW() / .985 - boxW / 1,ScrH() - boxH * 1.4, boxW, boxH)
+	surface.DrawText( NameVar )
 
 end)		
 		--Hide Default Hud--
